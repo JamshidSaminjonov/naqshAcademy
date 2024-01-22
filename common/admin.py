40a7712e18048ace870a7e22472e1de02f84dcb7
Jamshid <jsaminjonov5705@gmail.com>
2024-01-22 18:03:42 +0500

    commit

--- common/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-
-from common.models import Gallery, Variable
-
-admin.site.register(Gallery)
-admin.site.register(Variable)
--- common/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-
-from common.views import IndexView
-
-urlpatterns = [
-    path('', IndexView.as_view(), name='index')
-]
