0135eb8a6f2c0a1d70098212d866516556c3e64b
Jamshid <jsaminjonov5705@gmail.com>
2024-01-29 13:04:31 +0500

    add courses pages

--- js/main.js
+++ js/main.js
@@ -16,12 +16,47 @@ function sendEmail() {
   };
   emailjs.send("service_jcoke8p", "template_lybr56n", params).then(
     function (response) {
-      console.log("Email sent successfully:", response);
+      alert("Email sent successfully");
       // You can add any success handling here
+      document.getElementById("name").value = "";
+      document.getElementById("email").value = "";
+      document.getElementById("message").value = "";
     },
     function (error) {
-      console.log("Email sending failed:", error);
+      alert("Email sending failed");
       // You can add any error handling here
     }
   );
+}
+function goToAnotherPage(type) {
+  // Sizning boshqa sahifangizning URL'ini quyidagidek belgilang
+  switch (type) {
+    case "itforkids":
+      var targetPageURL = "courses/itforkids.html";
+      break;
+    case "frontend":
+      var targetPageURL = "courses/frontend.html";
+      break;
+    case "ingliz":
+      var targetPageURL = "courses/ingliz.html";
+      break;
+    case "python":
+      var targetPageURL = "courses/python.html";
+      break;
+    case "fstack":
+      var targetPageURL = "courses/fstack.html";
+      break;
+    case "mstack":
+      var targetPageURL = "courses/mstack.html";
+      break;
+    case "vdizayn":
+      var targetPageURL = "courses/vdizayn.html";
+      break;
+    case "gdizayn":
+      var targetPageURL = "courses/gdizayn.html";
+      break;
+    default:
+      break;
+  }
+  window.location.href = targetPageURL;
 }
